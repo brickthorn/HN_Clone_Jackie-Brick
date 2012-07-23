4f6849a6a91d971556614619521a9396681dd3b9
Apprentice <apprentice@devbootcamp.com>
2012-07-23 10:38:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JackieBrick::Application.config.secret_token = '0196be4dd6b0a6e7914b5d2a5af8d47d05bafdf172e728538e978b0f8c8e869632ab319c067416f6cf6022805f4bc401322614921393f2e88fff207abe44fad8'
